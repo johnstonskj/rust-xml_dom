f16d909986bd16073d53f8a7bbb71be2c911fcc3
Simon Johnston <johnstonskj@gmail.com>
2020-03-13 11:23:29 -0700

    Stubs for node tests

---
+++ tests/node.rs
@@ -0,0 +1,27 @@
+#[test]
+#[ignore]
+fn test_append_child_node() {}
+
+#[test]
+#[ignore]
+fn test_insert_child_node() {}
+
+#[test]
+#[ignore]
+fn test_replace_child_node() {}
+
+#[test]
+#[ignore]
+fn test_remove_child_node() {}
+
+#[test]
+#[ignore]
+fn test_child_node_navigation() {}
+
+#[test]
+#[ignore]
+fn test_clone_node() {}
+
+#[test]
+#[ignore]
+fn test_normalize() {}
